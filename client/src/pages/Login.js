1ead1f25a33db17805044fcf7638d84cf009edd6
Blake Georgeff <georgeffb@hotmail.com>
2021-11-06 10:44:55 +0800

    signup/login needs fixing

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,21 +1,89 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { Box, Flex, FormControl, Input, VStack, Heading, Button, Link } from '@chakra-ui/react';
+import { Text, Box, Flex, FormControl, Input, VStack, Heading, Button, Link } from '@chakra-ui/react';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 export default function Login() {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: '',
+      password: '',
+    });
+  };
+    
+    
     return (
         <Box w='100%' px={10} py={20} bgGradient="linear(to-r, blue.100, cyan.300)" >
             <Flex h='100vh' py={10}>
                 <VStack w='full' spacing={4} bg='blue.500' h='65%' p={4} borderRadius={20}>
                     <Heading>Login</Heading>
-                    <FormControl borderColor='black' borderWidth={2} borderRadius={10} w='80%'>
-                        <Input type='email' placeholder='Email'/>
-                    </FormControl>
-                    <FormControl borderColor='black' borderWidth={2} borderRadius={10} w='80%'>
-                        <Input type='password' placeholder='Password'/>
-                    </FormControl>
-                    <Button size='md' w='80%' >Login</Button>
-                    <Link href='/signup'>Don't already have an account? Signup here!</Link>
+                    {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+                    <form onSubmit={handleFormSubmit}>                    
+                        <FormControl isRequired borderColor='black' borderWidth={2} borderRadius={10} w='80%'>
+                            <Input 
+                                placeholder='Email'
+                                name='email'
+                                type='email'
+                                value={formState.email}
+                                onChange={handleChange}
+                            />
+                        </FormControl>
+                        <FormControl isRequired borderColor='black' borderWidth={2} borderRadius={10} w='80%'>
+                            <Input
+                                placeholder='Password'
+                                name='password'
+                                type='password'
+                                value={formState.password}
+                                onChange={handleChange}
+                            />
+                        </FormControl>
+                        <Button
+                            size='md' w='80%' 
+                            type='submit'
+                        > 
+                            Login
+                        </Button>
+                    </form>
+            )}
+                        {error && (
+                            <Text p={3} bg='red.500' color='white'>
+                                {error.message}
+                            </Text>
+                        )}
+                    <Link href='/signup'>Don't already have an account? Signip here!</Link>
                 </VStack>
             </Flex>
         </Box>
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -40,46 +40,55 @@ export default function Signup() {
     return (
         <Box w='100%' px={10} py={20} bgGradient="linear(to-r, blue.100, cyan.300)" >
             <Flex h='100vh' py={10}>
-                <VStack onSubmit={handleFormSubmit} w='full' spacing={4} bg='blue.500' h='75%' p={4} borderRadius={20}>
+                <VStack w='full' spacing={4} bg='blue.500' h='75%' p={4} borderRadius={20}>
                     <Heading>Signup</Heading>
-                    <FormControl borderColor='black' borderWidth={2} borderRadius={10} w='80%'>
-                        <Input 
-                            placeholder='Username'
-                            name='username'
-                            type='text'
-                            value={formState.name}
-                            onChange={handleChange}                            
+                    {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+                    <form onSubmit={handleFormSubmit}>                    
+                        <FormControl isRequired borderColor='black' borderWidth={2} borderRadius={10} w='80%'>
+                            <Input 
+                                placeholder='Username'
+                                name='username'
+                                type='text'
+                                value={formState.name}
+                                onChange={handleChange}                            
                             />
-                    </FormControl>
-                    <FormControl borderColor='black' borderWidth={2} borderRadius={10} w='80%'>
-                        <Input 
-                            placeholder='Email'
-                            name='email'
-                            type='email'
-                            value={formState.email}
-                            onChange={handleChange}
+                        </FormControl>
+                        <FormControl isRequired borderColor='black' borderWidth={2} borderRadius={10} w='80%'>
+                            <Input 
+                                placeholder='Email'
+                                name='email'
+                                type='email'
+                                value={formState.email}
+                                onChange={handleChange}
                             />
-                    </FormControl>
-                    <FormControl borderColor='black' borderWidth={2} borderRadius={10} w='80%'>
-                        <Input
-                            placeholder='Password'
-                            name='password'
-                            type='password'
-                            value={formState.password}
-                            onChange={handleChange}
+                        </FormControl>
+                        <FormControl isRequired borderColor='black' borderWidth={2} borderRadius={10} w='80%'>
+                            <Input
+                                placeholder='Password'
+                                name='password'
+                                type='password'
+                                value={formState.password}
+                                onChange={handleChange}
                             />
-                    </FormControl>
-                    <Button
-                        size='md' w='80%' 
-                        type='submit'
-                    > 
-                        Signup
-                    </Button>
-                    {error && (
-                        <Text p={5} bg='red.500' color='white'>
-                            {error.message}
-                        </Text>
-                    )}
+                        </FormControl>
+                        <Button
+                            size='md' w='80%' 
+                            type='submit'
+                        > 
+                            Signup
+                        </Button>
+                    </form>
+            )}
+                        {error && (
+                            <Text p={3} bg='red.500' color='white'>
+                                {error.message}
+                            </Text>
+                        )}
                     <Link href='/login'>Already have an account? Login here!</Link>
                 </VStack>
             </Flex>
