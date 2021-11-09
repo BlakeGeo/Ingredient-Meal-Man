b9d4969730f2c16696588803560b6ba2eb5ac58c
Blake Georgeff <georgeffb@hotmail.com>
2021-11-09 22:19:14 +0800

    update footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { FaGithub } from 'react-icons/fa';
+import { IconButton } from '@chakra-ui/react';
 
 const Footer = () => (
   <footer>
-      <h3>&copy; 2021 Blake Georgeff | Ingredient Meal Man</h3>
+      <h3>&copy; 2021 Blake Georgeff | Ingredient Meal Man
+        <IconButton 
+          as='a'
+          href='https://github.com/BlakeGeo/Ingredient-Meal-Man' 
+          aria-label='github repo'
+          icon={<FaGithub/>} 
+          bgColor='cyan.300' 
+          boxShadow='none!important'
+        />
+      </h3>
   </footer>
 );
 
