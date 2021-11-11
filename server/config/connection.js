108cd74c6cf366e1b41c2f6d6540f8a4f48a895e
Blake Georgeff <georgeffb@hotmail.com>
2021-11-11 17:20:32 +0800

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/ingedient-meal-man',
+  process.env.MONGODB_URI || 'mongodb://localhost/ingredient-meal-man',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
